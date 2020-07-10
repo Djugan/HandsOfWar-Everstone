a42d706d1d3bfa6c7574baf3a3c86e21da8182a5
edugan2 <43380700+edugan2@users.noreply.github.com>
2020-07-10 17:16:29 -0400

    Quest System

---
+++ Assets/Scripts/Quests/TestingData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TestingData : MonoBehaviour
+{
+    [SerializeField] private QuestData quest;
+    [SerializeField] private TextMeshProUGUI questName;
+    [SerializeField] private TextMeshProUGUI questDescription;
+    [SerializeField] private TextMeshProUGUI questObj1;
+    [SerializeField] private TextMeshProUGUI questObj2;
+    [SerializeField] private TextMeshProUGUI questObj3;
+    [SerializeField] private QuestObjectiveData objectives1;
+    [SerializeField] private QuestObjectiveData objectives2;
+    [SerializeField] private QuestObjectiveData objectives3;
+
+
+    private void Awake()
+    {
+        QuestWindow();
+    }
+
+    public void QuestWindow()
+    {
+        questName.text = quest.questName;
+        questDescription.text = quest.questDescription;
+        questObj1.text = objectives1.updateText;
+        questObj2.text = objectives2.updateText;
+        questObj3.text = objectives3.updateText;
+    }
+}
